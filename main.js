89525d027840f7bfc2333163f47c6832bc9e97c2
Gaurav Chandrashekar <cggaurav@gmail.com>
2013-11-20 00:37:44 +0100

    Calm:v1

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 /**
  * Listens for the app launching then creates the window
  *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('calm.html', {
-    id: "wrapper",
-    bounds: {
-      width: 700,
-      height: 500
-    },
-    maxWidth: 700,
-    minWidth: 700,
-    minHeight: 500,
-    maxHeight: 500,
-  });
+  runApp();
 });
+
+/**
+ * Listens for the app restarting then re-creates the window.
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ */
+chrome.app.runtime.onRestarted.addListener(function() {
+  runApp();
+});
+
+/**
+ * Creates the window for the application.
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+function runApp() {
+  chrome.app.window.create('calm.html', {
+    id: "browserWinID",
+    bounds: {
+      'width': 1024,
+      'height': 768
+    }
+  });
+}
